2c090355ea3e36a3e4af1284c5b04ff8a288a7fb
Nokib Sarkar <39087050+nokibsarkar@users.noreply.github.com>
2021-02-27 23:29:36 +0600

    Update init.py

--- init.py
+++ init.py
@@ -28,10 +28,34 @@ SPECIAL_TAGS = [
 ] 
 SPECIAL_TAG_COUNT = len(SPECIAL_TAGS)
 REGEXP_CLASS = re.compile("only for getting the class").__class__ # the class for confirming if an element is actually a regexp or not
-NAMESPACE_COUNT = 15 #total number of namespace
+
 NAMESPACE_MAPPER = {
-    
+    0 : 0, #(প্রধান/নিবন্ধ) 
+    1 : 1, #আলাপ 	১
+    2 : 2, #২ ব্যবহারকারী
+    3 : 3, #ব্যবহারকারী আলাপ 	৩
+    4 : 4, #৪ উইকিপিডিয়া
+    5 : 5, #উইকিপিডিয়া আলোচনা 	৫
+    6 : 6, #৬ 	চিত্র 
+    7 : 7, #চিত্র আলোচনা 	৭
+    8 : 8, #৮ 	মিডিয়াউইকি 
+    9 : 9, #িডিয়াউইকি আলোচনা৯
+    10 : 10, #১০ টেমপ্লেট 
+    11 : 11, #টেমপ্লেট আলোচনা১১
+    12 : 12, #১২ 	সাহায্য 
+    13 : 13, #সাহায্য আলোচনা 	১৩
+    14 : 14, #১৪ 	বিষয়শ্রেণী 
+    15 : 15, #বিষয়শ্রেণী আলোচনা 	১৫
+    100 : 16, #১০০ 	প্রবেশদ্বার 
+    101 : 17, #প্রবেশদ্বার আলোচনা 	১০১
+    828: 18, #৮২৮ 	মডিউল 
+    829 : 19, #মডিউল আলাপ 	৮২৯
+    2300 : 20, #২৩০০ 	গ্যাজেট 
+    2301 : 21, #গ্যাজেট আলোচনা 	২৩০১
+    2302 : 22, #২৩০২ 	গ্যাজেট সংজ্ঞা
+    2303 : 23 #গ্যাজেট সংজ্ঞার আলোচনা 	২৩০৩
 }
+NAMESPACE_COUNT = len(NAMESPACE_MAPPER) #total number of namespace
 ILLEGAL_USERNAME = None #the regexp pattern for illegal username
 CACHE_NAME = "cache.db" #sqlite3 database for cache
 CACHE_INIT = r"""CREATE TABLE  IF NOT EXISTS Contents (
@@ -55,11 +79,10 @@ FEATURE_COUNT = (
     + 1 + 1 # for url count in both revision
     + 1 + 1 # for email count in both revision
     + 1 + 1 # for phone count in both revision
-    + 1  # for illegal word count in username
     + 1 # for byte differnece of revision
     + 1  # for seconds differnece between two consecutive edit
-    + 1 ## tla = total link added
-    + 1  ## tlr = total link removed
+    + 1   ## tla = total link added
+    + 1    ## tlr = total link removed
      + 1   ## tea = total external link added
      + 1   ## ter = total external link removed
      + 1   ## tea/tla
@@ -68,10 +91,21 @@ FEATURE_COUNT = (
      + 1   ## tal = total added line
      + 1   ## twr = total word removed
      + 1   ## twa = total word added
-    + 1 + 1 # for url count in both revision
-    + 1 + 1 # for url count in both revision
-    + 1 + 1 # for url count in both revision
-    + 1 + 1 # for url count in both revision
-    + 1 + 1 # for url count in both revision
+    + 1 # for revision id difference
+    + 1 # if the title contains '/' literal i.e in the subpage
+    + 1 # whether previous edit was made by me
+    + 1 # for whether the editor is from IP
+    # if from IP then all of the below will be zero
+    + 1 # for similarity between title and username
+    + 1 # for similary between username and comment
+    + 1  # for illegal word count in username
 )
+OUTPUT_ROLLBACK = 0 #rollback the edit
+OUTPUT_NOTIFY = 1 # notify the user
+OUTPUT_OFFENSIVE = 2 # the edit was an offensive
+OUTPUT_COUNT = 3
+FETCH_REQUEST_PARAMS = [
+    ("action", "query"),
+    ("format", "json")
+]
 FEATURE_COUNT
