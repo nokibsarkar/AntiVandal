b2ad2ecb7f80f8ed47665351c88e127b4b22a622
Nazmul Haque Naqib <nokibsarkar@gmail.com>
2023-09-14 21:27:15 +0600

    added

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, Response
+from flask import Flask, request, Response, render_template
 from flask_cors import CORS, cross_origin
 import os
 app = Flask(__name__)
@@ -13,15 +13,43 @@ def deploy(token):
     else:
         return Response(status=404)
 
+
+
+
+
+
 @app.post("/sample/<int:user_id>")
 @cross_origin(origins="*")
 def collect_sample(user_id):
     return str(user_id)
 
 
+@app.get('/subscribe')
+def subscription_details():
+    return render_template('subscribe.html')
+
+@app.post('/subscribe')
+def subscribe():
+    return "Subscribed"
+
+
+@app.get('/unsubscribe/<string:token>')
+def unsubscribe_details():
+    return render_template('unsubscribe.html')
+
+@app.post('/unsubscribe/<int:user_id>')
+def unsubscribe(user_id):
+    return str(user_id)
+@app.route('/terms')
+def terms():
+    return render_template('terms.html')
+
+
+
+
 @app.route('/')
 def home():
-    return "Hello World!"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
